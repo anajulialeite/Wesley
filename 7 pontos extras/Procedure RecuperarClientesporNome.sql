df5284182a5c4d9f8dc4d9b6087ad5db8d7d8f2f
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-04-30 00:27:33 -0300

    Update Procedure RecuperarClientesporNome.sql

--- 7 pontos extras/Procedure RecuperarClientesporNome.sql
+++ 7 pontos extras/Procedure RecuperarClientesporNome.sql
@@ -1,27 +1,6 @@
-CREATE PROCEDURE RecuperarClientesPorNome
-    @NomeCliente NVARCHAR(100)
+CREATE PROCEDURE InsertRecuperarClientesPorNome
+    @NomeCliente VARCHAR(100)
 AS
 BEGIN
-    SET NOCOUNT ON;
-
-    SELECT 
-        IDCliente,
-        NomeCliente,
-        Endereco,
-        Telefone,
-        Email,
-        VeiculosRegistrados,
-        PlacasVeiculo,
-        MarcaVeiculo,
-        ModeloVeiculo,
-        AnoVeiculo,
-        UltimoServicoRealizado,
-        DataUltimoServico,
-        ProximoServicoAgendado,
-        DataProximoServico,
-        ObservacoesAdicionais
-    FROM 
-        Clientes
-    WHERE 
-        NomeCliente LIKE '%' + @NomeCliente + '%';
+    SELECT * FROM Cliente WHERE Nome = @NomeCliente
 END;
